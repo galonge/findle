b006c4d0943575056d0d8f8eb9d0e8f6b24177f7
George Alonge <georgedelson@gmail.com>
2012-12-23 20:57:53 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Findle::Application.config.secret_token = '442aeb9255dce96689f0e168516682db30a0b93fb473a1cc13a52e1d22cf57a0a6e5102840523cd8529696e0e9c703c7f0114eaa448dda15498c2cfcc8cd068f'
